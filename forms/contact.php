bdd770b1ce4be1a1dd01b5d397abe7fbe8c7937f
zhake <baukey.zh@gmail.com>
2023-09-20 03:50:53 +0600

    added new images v4

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,6 @@
 <?php
-  /**
+try {
+ /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
@@ -7,7 +8,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'info@bubbletea.kz';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,7 +18,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -38,4 +39,9 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+}
+catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
 ?>
